cf59f66ca78f54814d76ed851cd15d1c97dd529f
Fireryred <39081263+Fireryred@users.noreply.github.com>
2022-02-03 04:42:37 +0800

    Redirect auto to maps if rider has a delivery,

--- app/screens/rider/Fetch.js
+++ app/screens/rider/Fetch.js
@@ -1,6 +1,10 @@
 import React, {Component} from 'react';
-import {Button, ScrollView, View} from 'react-native';
+import {ScrollView} from 'react-native';
+import ReactNativeForegroundService from '@supersami/rn-foreground-service';
+
 import FetchRequest from '../../components/FetchRequest';
+
+import auth from '@react-native-firebase/auth';
 import firestore from '@react-native-firebase/firestore';
 
 export class Fetch extends Component {
@@ -8,9 +12,15 @@ export class Fetch extends Component {
     super();
     this.state = {
       requests: {},
+      data: {},
+      donorDetails: {},
+      exists: false,
     };
   }
   componentDidMount() {
+    if (ReactNativeForegroundService.is_running) {
+      ReactNativeForegroundService.stop();
+    }
     this.props.navigation.getParent().setOptions({title: 'Fetch'});
 
     let unsubscribe = this.props.navigation.addListener('tabPress', e => {
@@ -18,13 +28,48 @@ export class Fetch extends Component {
       // e.preventDefault();
       this.props.navigation.getParent().setOptions({title: 'Fetch'});
     });
+    this.checkRiderStatus().catch(error => {
+      console.error();
+    });
     this.getRequests().catch(error => {
       console.error();
     });
   }
+  async checkRiderStatus() {
+    let data = {};
+    let exists = false;
+    let donor = {};
+    const fetchRequest = await firestore()
+      .collection('fetch_requests')
+      .where('fetcherId', '==', auth().currentUser.uid)
+      .where('status', 'in', ['pickup', 'transit'])
+      .get();
+    fetchRequest.forEach(doc => {
+      if (doc.exists) {
+        data = [doc.id, doc.data()];
+        firestore()
+          .collection('users')
+          .doc(doc.data().donorID)
+          .get()
+          .then(doc => {
+            exists = doc.exists;
+            donor = doc;
+            this.setState({
+              data: {...data},
+              donorDetails: {...donor},
+              exists: exists,
+            });
+          });
+      }
+    });
+  }
+
   async getRequests() {
     const requests = {};
-    const fetchRequest = await firestore().collection('fetch_requests').get();
+    const fetchRequest = await firestore()
+      .collection('fetch_requests')
+      .where('status', '==', 'waiting')
+      .get();
     fetchRequest.forEach(doc => {
       requests[doc.id] = doc.data();
     });
@@ -32,14 +77,28 @@ export class Fetch extends Component {
   }
 
   toMaps = (data, donorDetails) => {
+    this.updateStatus(data);
+    data[1].status = 'pickup';
+    console.log(data);
     this.props.navigation.navigate('Maps', {
       data: data,
       donorDetails: donorDetails,
     });
   };
 
+  async updateStatus(data) {
+    await firestore()
+      .collection('fetch_requests')
+      .doc(data[0])
+      .update({status: 'pickup', fetcherId: auth().currentUser.uid})
+      .then(() => console.log('Status Updated to pickup'));
+  }
+
   render() {
-    const {requests} = this.state;
+    const {requests, data, donorDetails, exists} = this.state;
+    if (exists) {
+      this.toMaps(data, donorDetails);
+    }
     return (
       <ScrollView>
         {Object.entries(requests).map((request, key) => (
