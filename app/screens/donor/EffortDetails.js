095bd6e5a64739811c84877d9e8674ec988581e9
rem-iri <remer.irineo@gmail.com>
2022-02-04 00:39:41 +0800

    Dev: Donor Donation Effort Page

--- app/screens/donor/EffortDetails.js
+++ app/screens/donor/EffortDetails.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
-import { StyleSheet, View, StatusBar, SafeAreaView } from 'react-native'
-
-import { Text, Button} from 'react-native-paper'
+import { StyleSheet, View, StatusBar, SafeAreaView, ScrollView, Image } from 'react-native'
 
 import firestore from '@react-native-firebase/firestore';
+
+import { TextInput, Button, Text, Checkbox, Surface, Subheading, RadioButton, Portal, Provider, Title, Headline} from 'react-native-paper'
+
+import FontAwesome from 'react-native-vector-icons/FontAwesome5';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 export default class EffortDetails extends Component {
     constructor() {
@@ -19,7 +22,13 @@ export default class EffortDetails extends Component {
             endDateTime: null,
             effortCoordinates: null,
             geocodeAddress: null,
-            city: null
+            city: null,
+            imageUrl: null,
+            csoData: {
+                email: null,
+                organizationName: null,
+                username: null,
+            }
         }
     }
     componentDidMount() {
@@ -44,38 +53,85 @@ export default class EffortDetails extends Component {
 
         this.setState({
             ...this.state,
-            title: effort.title, 
-            description: effort.description, 
-            csoID: effort.csoID, 
-            creationDate: effort.creationDate, 
-            startDateTime: effort.startDateTime, 
-            endDateTime: effort.endDateTime, 
-            effortCoordinates: effort.location, 
+            title: effort.title,
+            description: effort.description,
+            csoID: effort.csoID,
+            creationDate: effort.creationDate,
+            startDateTime: effort.startDateTime,
+            endDateTime: effort.endDateTime,
+            effortCoordinates: effort.location,
             geocodeAddress: effort.geocodeAddress,
-            city: effort.city
+            city: effort.city,
+            imageUrl: effort.imageUrl,
         }, () => {
             console.log("effort state", this.state);
+            this.loadCSOData()
+                .catch((err) => {
+                    console.log("error getting cso data", err)
+                    this.props.navigation.replace("DonorDrawer")
+                })
         })
     }
 
+    loadCSOData = async () => {
+        let {csoID} = this.state;
+        let doc = await firestore().collection("users").doc(csoID).get();
+        let csoData = doc.data();
+
+        this.setState({
+            ...this.state,
+            csoData: {...csoData}
+        }, () => console.log(csoData))
+    }
+
     render() {
-        let {effortId, effortCoordinates, geocodeAddress, city} = this.state
+        let { effortId, effortCoordinates, geocodeAddress, city } = this.state;
+        let { title, description, creationDate, startDateTime, imageUrl } = this.state;
+        let {organizationName} = this.state.csoData;
 
         return (
-            <View>
-                <Text style={{ color: '#fff' }}>Effort Details</Text>
-                {Object.entries(this.state).map((value, index) => { return <><Text>{value.toString()}</Text></> })}
-                <Button
-                    onPress={() => {
-                        this.props.navigation.navigate("RequestFetch", {effortId, effortCoordinates, geocodeAddress, city})
-                    }}
-                >Request Fetch</Button>
-            </View>
+            <ScrollView style={styles.container}>
+                <View style={styles.segment}>
+                    <Text style={{ color: '#fff' }}>Effort Details</Text>
+                    <Headline style={{fontSize: 30}}>{title}</Headline>
+                    <Text>By {organizationName} <MaterialCommunityIcons color="blue" name="check-decagram"></MaterialCommunityIcons></Text>
+                </View>
+                <View style={styles.segment}>
+                    <Text style={{color: "#666666", fontWeight: "bold"}}>DESCRIPTION</Text>
+                    <Text>{description}</Text>
+                </View>
+                
+                <View style={{...styles.segment, }}>
+                    <Text style={{color: "#666666", fontWeight: "bold"}}>MEDIA</Text>
+                    <Image resizeMode="contain" style={{ height: 400, width: null, backgroundColor: "rgba(0,0,0,.1)" }} source={{uri: imageUrl}} />
+                </View>
+
+                <View style={{...styles.segment, display: "flex", flexDirection: "column", alignItems: "center"}}>
+                    <Text style={{color: "#666666", fontWeight: "bold"}}>FOR ONLINE CASH DONATIONS</Text>
+                    <Text>View CSO Information Page</Text>
+                </View>
+                {/* {Object.entries(this.state).map((value, index) => { return <><Text>{value.toString()}</Text></> })} */}
+                <View style={{...styles.segment, display: "flex", flexDirection: "column", alignItems: "center"}}>
+                    <Text style={{color: "#666666", fontWeight: "bold", textAlign: "center"}}>FOR IN-KIND DONATIONS</Text>
+                    <Button
+                        mode="contained"
+                        onPress={() => {
+                            this.props.navigation.navigate("RequestFetch", { effortId, effortCoordinates, geocodeAddress, city })
+                        }}
+                    >DONATE ITEMS ❤</Button>
+                </View>
+            </ScrollView>
 
         )
     }
 }
 
 const styles = StyleSheet.create({
-
+    container: {
+        padding: 10
+    },
+    segment: {
+        marginBottom: 20,
+        display: "flex"
+    }
 })
