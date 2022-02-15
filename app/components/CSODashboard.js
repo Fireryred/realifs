427529bbd6cb5fe5d9980f030895ce3f094fb560
Fireryred <39081263+Fireryred@users.noreply.github.com>
2022-02-15 20:38:23 +0800

    Added Delete Button

--- app/components/CSODashboard.js
+++ app/components/CSODashboard.js
@@ -10,8 +10,9 @@ class CSODashboard extends Component {
   }
 
   componentDidMount() {
+    console.log(this.props);
     this.getNoOfDeliveries().catch(error => {
-      console.log(errorr);
+      console.log(error);
     });
     this.formatDate();
   }
@@ -59,10 +60,17 @@ class CSODashboard extends Component {
     this.setState({date: cdate});
   }
 
+  async updateDonoEff() {
+    const {data} = this.props;
+    await firestore()
+      .collection('donation_efforts')
+      .doc(data[0])
+      .update({isDeleted: true});
+  }
   render() {
     const {data, gotoDono} = this.props;
     const {noOfDeliveries, date} = this.state;
-    let button = data[1].isDelivered ? 'View' : 'Edit';
+    let button = data[1].isDeleted ? 'View' : 'Edit';
     return (
       <View>
         <Card>
@@ -71,6 +79,11 @@ class CSODashboard extends Component {
             <Text>STARTED {date}</Text>
             <Text>Address: {data[1].geocodeAddress}</Text>
             <Text>DELIVERIES RECIEVED: {noOfDeliveries}</Text>
+            {!data[1].isDeleted && (
+              <Button title="delete" onPress={() => this.updateDonoEff()}>
+                Delete
+              </Button>
+            )}
             <Button title={button} onPress={() => gotoDono(data)}>
               {button}
             </Button>
