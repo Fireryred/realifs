fcf6a813c6cb5609a3d6b0778db53cce0534511c
Fireryred <39081263+Fireryred@users.noreply.github.com>
2022-02-28 12:14:38 +0800

    Add: Cash-in minimum amount guard

--- app/screens/rider/CashIn.js
+++ app/screens/rider/CashIn.js
@@ -14,6 +14,8 @@ class CashIn extends Component {
     const balance = parseInt(this.props.route.params.balance);
     if (amount <= 0 || amount === '' || amount % 1 !== 0) {
       Alert.alert('Please enter a valid amount');
+    } else if (amount < 10000) {
+      Alert.alert('Please enter a minimum amount of \u20B1100');
     } else {
       this.props.navigation.reset({
         index: 1,
@@ -33,18 +35,40 @@ class CashIn extends Component {
   };
   render() {
     return (
-      <View style={{flex: 1, display: "flex", justifyContent: "center", padding: 10}}>
-        <Text style={{fontWeight: "bold", fontSize: 18}}>Cash-in Amount</Text>
+      <View
+        style={{
+          flex: 1,
+          display: 'flex',
+          justifyContent: 'center',
+          padding: 10,
+        }}>
+        <Text style={{fontWeight: 'bold', fontSize: 18}}>Cash-in Amount</Text>
         <TextInput
-          mode='outlined'
+          mode="outlined"
           keyboardType="numeric"
           onChangeText={text => this.setState({amount: text})}
         />
-        <View style={{display: "flex", flexDirection: "row", justifyContent: "center"}}>
-          <Button style={{margin: 10}} mode='contained' color='gray' dark={true} title="CANCEL" onPress={() => this.props.navigation.goBack()}>
+        <View
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'center',
+          }}>
+          <Button
+            style={{margin: 10}}
+            mode="contained"
+            color="gray"
+            dark={true}
+            title="CANCEL"
+            onPress={() => this.props.navigation.goBack()}>
             CANCEL
           </Button>
-          <Button style={{margin: 10}} mode='contained' color='green' title="CASH-IN" onPress={this.handleCashIn}>
+          <Button
+            style={{margin: 10}}
+            mode="contained"
+            color="green"
+            title="CASH-IN"
+            onPress={this.handleCashIn}>
             CASH-IN
           </Button>
         </View>
